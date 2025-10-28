56643695ac7e7971c4b71b4181f893555c722a74
kittiphops <kittiphopsompuech2@gmail.com>
2025-10-28 11:43:50 +0700

    fix:update modal

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,11 +6,3 @@ export * from './ui';
 // Layout Components
 export { default as Navbar } from './layout/Navbar';
 export { default as Header } from './layout/Header';
-
-// Auth Components are now in contexts
-
-// Activity Components
-export { default as ActivityCard } from './activities/ActivityCard';
-
-// Chat Components
-export { default as ChatMessage } from './chat/ChatMessage';
